26d70debc11ce5b2b369a81cd96ccba2ba6ebd81
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-12-05 17:17:23 +0300

    refactor(api): add storage and image configuration to api

--- api/v1alpha1/robot_types.go
+++ api/v1alpha1/robot_types.go
@@ -1,15 +1,57 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// ROS distro selection. Allowed distros are Foxy and Galactic. It is aimed to support Humble, Melodic and Noetic in further versions.
+// +kubebuilder:validation:Enum=foxy;galactic
+type ROSDistro string
+
+const (
+	// ROS Melodic Morenia
+	ROSDistroMelodic ROSDistro = "melodic"
+	// ROS Noetic Ninjemys
+	ROSDistroNoetic ROSDistro = "noetic"
+	// ROS Foxy Fitzroy
+	ROSDistroFoxy ROSDistro = "foxy"
+	// ROS Galactic Geochelone
+	ROSDistroGalactic ROSDistro = "galactic"
+)
+
+// Storage class configuration for a volume type.
+type StorageClassConfig struct {
+	// Storage class name
+	Name string `json:"name,omitempty"`
+	// PVC access mode
+	AccessMode corev1.PersistentVolumeAccessMode `json:"accessMode,omitempty"`
+}
+
+// Robot's resource limitations.
+type Storage struct {
+	// Specifies how much storage will be allocated in total.
+	// +kubebuilder:default=10000
+	Amount int `json:"amount,omitempty"`
+	// Storage class selection for robot's volumes.
+	StorageClassConfig StorageClassConfig `json:"storageClassConfig,omitempty"`
+}
+
 // RobotSpec defines the desired state of Robot
 type RobotSpec struct {
+	// ROS distro to be used.
+	// +kubebuilder:validation:Required
+	Distro ROSDistro `json:"distro"`
+	// Resource limitations of robot containers.
+	Storage Storage `json:"storage,omitempty"`
 }
 
 // RobotStatus defines the observed state of Robot
 type RobotStatus struct {
+	// Image of robot
+	Image string `json:"image,omitempty"`
+	// Node name
+	NodeName string `json:"nodeName,omitempty"`
 }
 
 //+kubebuilder:object:root=true
