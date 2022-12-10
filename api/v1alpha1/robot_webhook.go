856be0bd43c2c40d763a25d27678144893ddad51
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-12-11 02:43:45 +0300

    refactor(webhook): update robot webhooks

--- api/v1alpha1/robot_webhook.go
+++ api/v1alpha1/robot_webhook.go
@@ -1,6 +1,9 @@
 package v1alpha1
 
 import (
+	"errors"
+
+	"github.com/robolaunch/robot-operator/internal"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -24,7 +27,21 @@ var _ webhook.Defaulter = &Robot{}
 func (r *Robot) Default() {
 	robotlog.Info("default", "name", r.Name)
 
+	DefaultRepositoryPaths(r)
+
 	// TODO(user): fill in your defaulting logic.
+}
+
+func DefaultRepositoryPaths(r *Robot) {
+	for wsKey := range r.Spec.Workspaces {
+		ws := r.Spec.Workspaces[wsKey]
+		for repoKey := range ws.Repositories {
+			repo := ws.Repositories[repoKey]
+			repo.Path = r.Spec.WorkspacesPath + "/" + ws.Name + "/src/" + repo.Name
+			ws.Repositories[repoKey] = repo
+		}
+		r.Spec.Workspaces[wsKey] = ws
+	}
 }
 
 //+kubebuilder:webhook:path=/validate-robot-roboscale-io-v1alpha1-robot,mutating=false,failurePolicy=fail,sideEffects=None,groups=robot.roboscale.io,resources=robots,verbs=create;update,versions=v1alpha1,name=vrobot.kb.io,admissionReviewVersions=v1
@@ -34,17 +51,50 @@ var _ webhook.Validator = &Robot{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Robot) ValidateCreate() error {
 	robotlog.Info("validate create", "name", r.Name)
+
+	err := r.checkTenancyLabels()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Robot) ValidateUpdate(old runtime.Object) error {
 	robotlog.Info("validate update", "name", r.Name)
+
+	err := r.checkTenancyLabels()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *Robot) ValidateDelete() error {
 	robotlog.Info("validate delete", "name", r.Name)
+	return nil
+}
+
+func (r *Robot) checkTenancyLabels() error {
+	labels := r.GetLabels()
+
+	if _, ok := labels[internal.ORGANIZATION_LABEL_KEY]; !ok {
+		return errors.New("organization label should be added with key " + internal.ORGANIZATION_LABEL_KEY)
+	}
+
+	if _, ok := labels[internal.TEAM_LABEL_KEY]; !ok {
+		return errors.New("team label should be added with key " + internal.TEAM_LABEL_KEY)
+	}
+
+	if _, ok := labels[internal.REGION_LABEL_KEY]; !ok {
+		return errors.New("super cluster label should be added with key " + internal.REGION_LABEL_KEY)
+	}
+
+	if _, ok := labels[internal.CLOUD_INSTANCE_LABEL_KEY]; !ok {
+		return errors.New("cloud instance label should be added with key " + internal.CLOUD_INSTANCE_LABEL_KEY)
+	}
 	return nil
 }
