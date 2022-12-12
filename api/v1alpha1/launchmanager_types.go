4d48b204a45e5ee80eb857a16dd2a816bbf2615a
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-12-13 02:50:57 +0300

    refactor(api): add initial fields to launch manager

--- api/v1alpha1/launchmanager_types.go
+++ api/v1alpha1/launchmanager_types.go
@@ -17,15 +17,66 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type LaunchType string
+
+const (
+	LaunchTypeDistributed LaunchType = "Distributed"
+	LaunchTypeDirect      LaunchType = "Direct"
+)
+
+// Prelaunch command or script is applied just before the node is started.
+type Prelaunch struct {
+	// Bash command to run before ROS node execution.
+	// +kubebuilder:validation:Required
+	Command string `json:"command"`
+	// Script  string `json:"script,omitempty"`
+}
+
+// Launch description of a repository.
+type Launch struct {
+	// Name of the workspace.
+	Workspace string `json:"workspace"`
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Enum=Distributed;Direct
+	Type LaunchType `json:"type"`
+	// Additional environment variables to set when launching ROS nodes.
+	Env []corev1.EnvVar `json:"env,omitempty"`
+	// Path to launchfile in repository. (eg. `linorobot/linorobot_gazebo/launch.py`)
+	// +kubebuilder:validation:Required
+	LaunchFilePath string `json:"launchFilePath"`
+	// Launch parameters.
+	Parameters map[string]string `json:"parameters,omitempty"`
+	// Command or script to run just before node's execution.
+	Prelaunch Prelaunch `json:"prelaunch,omitempty"`
+}
+
 // LaunchManagerSpec defines the desired state of LaunchManager
 type LaunchManagerSpec struct {
+	Launch []Launch `json:"launch,omitempty"`
 }
+
+type LaunchPodStatus struct {
+	Created bool            `json:"created,omitempty"`
+	Phase   corev1.PodPhase `json:"phase,omitempty"`
+	IP      string          `json:"ip,omitempty"`
+}
+
+type LaunchManagerPhase string
+
+const (
+	LaunchManagerPhaseCreatingPod   LaunchManagerPhase = "Launching"
+	LaunchManagerPhaseReady         LaunchManagerPhase = "Ready"
+	LaunchManagerPhaseMalfunctioned LaunchManagerPhase = "Malfunctioned"
+)
 
 // LaunchManagerStatus defines the observed state of LaunchManager
 type LaunchManagerStatus struct {
+	Phase           LaunchManagerPhase `json:"phase,omitempty"`
+	LaunchPodStatus LaunchPodStatus    `json:"launchPodStatus,omitempty"`
 }
 
 //+kubebuilder:object:root=true
