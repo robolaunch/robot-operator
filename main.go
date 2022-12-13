a925afeeb449205e84da5eb5e139641083fa7a96
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-12-13 03:03:17 +0300

    refactor(webhook): add launch manager webhooks

--- main.go
+++ main.go
@@ -147,6 +147,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "LaunchManager")
 		os.Exit(1)
 	}
+	if err = (&robotv1alpha1.LaunchManager{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "LaunchManager")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
