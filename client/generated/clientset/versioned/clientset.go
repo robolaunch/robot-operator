166de78351d86b86c7c5f975ef0bd46a1c01726b
tunahanertekin <tunahan.ertekin13@gmail.com>
2022-12-19 10:17:45 +0300

    refactor(clientset): revert clientset changes, update controller-runtime

--- client/generated/clientset/versioned/clientset.go
+++ client/generated/clientset/versioned/clientset.go
@@ -32,7 +32,8 @@ type Interface interface {
 	RoboscaleV1alpha1() roboscalev1alpha1.RoboscaleV1alpha1Interface
 }
 
-// Clientset contains the clients for groups.
+// Clientset contains the clients for groups. Each group has exactly one
+// version included in a Clientset.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	roboscaleV1alpha1 *roboscalev1alpha1.RoboscaleV1alpha1Client
