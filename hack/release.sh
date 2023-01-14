401dbd66a260afd31a88d7cde089af0167fd651c
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-01-14 21:41:04 +0300

    build(manifests): :whale: update manifests for v0.1.6

--- hack/release.sh
+++ hack/release.sh
@@ -10,5 +10,5 @@ make docker-build docker-push gh-extract IMG=robolaunchio/robot-controller-manag
 make gh-select-node LABEL_KEY="robolaunch.io/organization" LABEL_VAL="robolaunch"
 make gh-select-node LABEL_KEY="robolaunch.io/team" LABEL_VAL="robotics"
 make gh-select-node LABEL_KEY="robolaunch.io/region" LABEL_VAL="europe-east"
-make gh-select-node LABEL_KEY="robolaunch.io/buffer-instance" LABEL_VAL="buffer"
 make gh-select-node LABEL_KEY="robolaunch.io/cloud-instance" LABEL_VAL="cluster"
+make gh-select-node LABEL_KEY="robolaunch.io/cloud-instance-alias" LABEL_VAL="cluster-alias"
