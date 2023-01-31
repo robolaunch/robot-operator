004083ffa54aaca392cecb1cd9453d71b31a9c52
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-01-31 12:41:44 +0300

    build(release): :whale: update release script

--- hack/release.sh
+++ hack/release.sh
@@ -6,7 +6,8 @@ if [ -z "$1" ]
     exit 1
 fi
 
-make docker-buildx docker-push gh-extract IMG=robolaunchio/robot-controller-manager:$1
+# make docker-build docker-push gh-extract IMG=robolaunchio/robot-controller-manager:$1
+make docker-buildx gh-extract IMG=robolaunchio/robot-controller-manager:$1
 make gh-select-node LABEL_KEY="robolaunch.io/organization" LABEL_VAL="robolaunch"
 make gh-select-node LABEL_KEY="robolaunch.io/team" LABEL_VAL="robotics"
 make gh-select-node LABEL_KEY="robolaunch.io/region" LABEL_VAL="europe-east"
