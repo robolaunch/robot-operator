f5a11fb4ebfa3f5daefad396a251c816f88bb1bd
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-02-23 16:42:55 +0300

    refactor(k3s): check if cluster is k3s

--- internal/node/node.go
+++ internal/node/node.go
@@ -8,3 +8,10 @@ func HasGPU(node corev1.Node) bool {
 	_, ok := node.Status.Allocatable["nvidia.com/gpu"]
 	return ok
 }
+
+func IsK3s(node corev1.Node) bool {
+	if val, ok := node.Labels["node.kubernetes.io/instance-type"]; ok && val == "k3s" {
+		return true
+	}
+	return false
+}
