f59ecdecd93e87afd362f83f2397a95f2c0623b3
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-02-27 11:08:19 +0300

    refactor(node): add node checkers

--- pkg/controllers/robot_dev_suite/robot_ide/helpers.go
+++ pkg/controllers/robot_dev_suite/robot_ide/helpers.go
@@ -3,10 +3,15 @@ package robot_ide
 import (
 	"context"
 
+	robotErr "github.com/robolaunch/robot-operator/internal/error"
 	"github.com/robolaunch/robot-operator/internal/label"
 	robotv1alpha1 "github.com/robolaunch/robot-operator/pkg/api/roboscale.io/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/retry"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func (r *RobotIDEReconciler) reconcileGetInstance(ctx context.Context, meta types.NamespacedName) (*robotv1alpha1.RobotIDE, error) {
@@ -60,4 +65,46 @@ func (r *RobotIDEReconciler) reconcileGetTargetRobotVDI(ctx context.Context, ins
 	}
 
 	return robotVDI, nil
+}
+
+func (r *RobotIDEReconciler) reconcileCheckNode(ctx context.Context, instance *robotv1alpha1.Robot) (*corev1.Node, error) {
+
+	tenancyMap := label.GetTenancyMap(instance)
+
+	requirements := []labels.Requirement{}
+	for k, v := range tenancyMap {
+		newReq, err := labels.NewRequirement(k, selection.In, []string{v})
+		if err != nil {
+			return nil, err
+		}
+		requirements = append(requirements, *newReq)
+	}
+
+	nodeSelector := labels.NewSelector().Add(requirements...)
+
+	nodes := &corev1.NodeList{}
+	err := r.List(ctx, nodes, &client.ListOptions{
+		LabelSelector: nodeSelector,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(nodes.Items) == 0 {
+		return nil, &robotErr.NodeNotFoundError{
+			ResourceKind:      instance.Kind,
+			ResourceName:      instance.Name,
+			ResourceNamespace: instance.Namespace,
+		}
+	} else if len(nodes.Items) > 1 {
+		return nil, &robotErr.MultipleNodeFoundError{
+			ResourceKind:      instance.Kind,
+			ResourceName:      instance.Name,
+			ResourceNamespace: instance.Namespace,
+		}
+	}
+
+	instance.Status.NodeName = nodes.Items[0].Name
+
+	return &nodes.Items[0], nil
 }
