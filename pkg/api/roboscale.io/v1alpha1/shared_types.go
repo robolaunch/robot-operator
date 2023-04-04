82dcc8e6add0b12f03bad91ea67311490ee24858
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-04-04 18:02:07 +0300

    refactor(status): remove omitempty from the bool field 'created'

--- pkg/api/roboscale.io/v1alpha1/shared_types.go
+++ pkg/api/roboscale.io/v1alpha1/shared_types.go
@@ -3,7 +3,7 @@ package v1alpha1
 import corev1 "k8s.io/api/core/v1"
 
 type OwnedResourceStatus struct {
-	Created   bool                   `json:"created,omitempty"`
+	Created   bool                   `json:"created"`
 	Reference corev1.ObjectReference `json:"reference,omitempty"`
 	Phase     string                 `json:"phase,omitempty"`
 }
