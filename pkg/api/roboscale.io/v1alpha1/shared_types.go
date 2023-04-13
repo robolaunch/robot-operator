4a278e78446c6bfde10e81ff09c686583f04edfb
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-04-13 13:14:28 +0300

    docs(shared): :books: complete field descriptions for shared types

--- pkg/api/roboscale.io/v1alpha1/shared_types.go
+++ pkg/api/roboscale.io/v1alpha1/shared_types.go
@@ -2,38 +2,54 @@ package v1alpha1
 
 import corev1 "k8s.io/api/core/v1"
 
+// Generic status for any owned resource.
 type OwnedResourceStatus struct {
-	Created   bool                   `json:"created"`
+	// Shows if the owned resource is created.
+	Created bool `json:"created"`
+	// Reference to the owned resource.
 	Reference corev1.ObjectReference `json:"reference,omitempty"`
-	Phase     string                 `json:"phase,omitempty"`
+	// Phase of the owned resource.
+	Phase string `json:"phase,omitempty"`
 }
 
 type OwnedPodStatus struct {
+	// Generic status for any owned resource.
 	Resource OwnedResourceStatus `json:"resource,omitempty"`
-	IP       string              `json:"ip,omitempty"`
+	// IP of the pod.
+	IP string `json:"ip,omitempty"`
 }
 
 type DiscoveryServerInstanceStatus struct {
-	Resource OwnedResourceStatus   `json:"resource,omitempty"`
-	Status   DiscoveryServerStatus `json:"status,omitempty"`
+	// Generic status for any owned resource.
+	Resource OwnedResourceStatus `json:"resource,omitempty"`
+	// Status of the DiscoveryServer instance.
+	Status DiscoveryServerStatus `json:"status,omitempty"`
 }
 
 type ROSBridgeInstanceStatus struct {
+	// Generic status for any owned resource.
 	Resource OwnedResourceStatus `json:"resource,omitempty"`
-	Status   ROSBridgeStatus     `json:"status,omitempty"`
+	// Status of the ROSBridge instance.
+	Status ROSBridgeStatus `json:"status,omitempty"`
 }
 
 type RobotDevSuiteInstanceStatus struct {
+	// Generic status for any owned resource.
 	Resource OwnedResourceStatus `json:"resource,omitempty"`
-	Status   RobotDevSuiteStatus `json:"status,omitempty"`
+	// Status of the RobotDevSuite instance.
+	Status RobotDevSuiteStatus `json:"status,omitempty"`
 }
 
 type WorkspaceManagerInstanceStatus struct {
-	Resource OwnedResourceStatus    `json:"resource,omitempty"`
-	Status   WorkspaceManagerStatus `json:"status,omitempty"`
+	// Generic status for any owned resource.
+	Resource OwnedResourceStatus `json:"resource,omitempty"`
+	// Status of the WorkspaceManager instance.
+	Status WorkspaceManagerStatus `json:"status,omitempty"`
 }
 
 type StepStatus struct {
+	// Generic status for any owned resource.
 	Resource OwnedResourceStatus `json:"resource,omitempty"`
-	Step     Step                `json:"step,omitempty"`
+	// Status of the step.
+	Step Step `json:"step,omitempty"`
 }
