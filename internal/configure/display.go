691bb6630f066742a4df0db8cbd21acebbd82602
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-05-08 16:58:56 +0300

    refactor(display): refactor display config injection

--- internal/configure/display.go
+++ internal/configure/display.go
@@ -8,35 +8,43 @@ import (
 
 func InjectPodDisplayConfiguration(pod *corev1.Pod, robotVDI robotv1alpha1.RobotVDI) *corev1.Pod {
 
-	placeDisplayEnvironmentVariables(pod)
-	placeDisplayVolume(pod, robotVDI)
+	configurePod(pod, robotVDI)
+	for k, container := range pod.Spec.Containers {
+		configureContainer(&container, robotVDI)
+		pod.Spec.Containers[k] = container
+	}
 
 	return pod
 }
 
-func placeDisplayEnvironmentVariables(pod *corev1.Pod) {
+func InjectLaunchPodDisplayConfiguration(pod *corev1.Pod, launchManager robotv1alpha1.LaunchManager, robotVDI robotv1alpha1.RobotVDI) *corev1.Pod {
 
+	configurePod(pod, robotVDI)
+	for launchName, l := range launchManager.Spec.Launches {
+		if l.Container.Display {
+			for k, container := range pod.Spec.Containers {
+				if container.Name == launchName {
+					configureContainer(&pod.Spec.Containers[k], robotVDI)
+				}
+			}
+		}
+	}
+
+	return pod
+}
+
+func configurePod(pod *corev1.Pod, robotVDI robotv1alpha1.RobotVDI) {
+	volume := GetVolumeX11Unix(&robotVDI)
+	pod.Spec.Volumes = append(pod.Spec.Volumes, volume)
+}
+
+func configureContainer(container *corev1.Container, robotVDI robotv1alpha1.RobotVDI) {
+	volume := GetVolumeX11Unix(&robotVDI)
+	volumeMount := GetVolumeMount(internal.X11_UNIX_PATH, volume)
 	environmentVariables := []corev1.EnvVar{
 		internal.Env("DISPLAY", ":0"),
 	}
 
-	for k, container := range pod.Spec.Containers {
-		container.Env = append(container.Env, environmentVariables...)
-		pod.Spec.Containers[k] = container
-	}
-
-}
-
-func placeDisplayVolume(pod *corev1.Pod, robotVDI robotv1alpha1.RobotVDI) {
-
-	volume := GetVolumeX11Unix(&robotVDI)
-	pod.Spec.Volumes = append(pod.Spec.Volumes, volume)
-
-	volumeMount := GetVolumeMount(internal.X11_UNIX_PATH, volume)
-
-	for k, container := range pod.Spec.Containers {
-		container.VolumeMounts = append(container.VolumeMounts, volumeMount)
-		pod.Spec.Containers[k] = container
-	}
-
+	container.VolumeMounts = append(container.VolumeMounts, volumeMount)
+	container.Env = append(container.Env, environmentVariables...)
 }
