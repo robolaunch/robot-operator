b8ff29774fb1bc0a1940e7bef725740696449995
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-07-31 17:18:34 +0300

    refactor(domain-id): add ros domain id injectors

---
+++ internal/configure/domain_id.go
@@ -0,0 +1,30 @@
+package configure
+
+import (
+	"strconv"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+func InjectPodROSDomainID(pod *corev1.Pod, domainID int) *corev1.Pod {
+
+	placeROSDomainIDEnvironmentVariables(pod, domainID)
+
+	return pod
+}
+
+func placeROSDomainIDEnvironmentVariables(pod *corev1.Pod, domainID int) {
+
+	environmentVariables := []corev1.EnvVar{
+		{
+			Name:  "ROS_DOMAIN_ID",
+			Value: strconv.Itoa(domainID),
+		},
+	}
+
+	for k, container := range pod.Spec.Containers {
+		container.Env = append(container.Env, environmentVariables...)
+		pod.Spec.Containers[k] = container
+	}
+
+}
