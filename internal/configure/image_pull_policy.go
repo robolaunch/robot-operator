90a462f817639eaeb964aaa57e9e992971404550
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-09-15 17:15:55 +0300

    refactor(image-pull-policy): update injector function signatures

--- internal/configure/image_pull_policy.go
+++ internal/configure/image_pull_policy.go
@@ -1,25 +1,24 @@
 package configure
 
 import (
-	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
-func InjectImagePullPolicyToPod(pod *corev1.Pod) *corev1.Pod {
+func InjectImagePullPolicy(pod *corev1.Pod) *corev1.Pod {
 
-	placeImagePullPolicyToPods(pod)
+	placeImagePullPolicy(pod)
 
 	return pod
 }
 
-func InjectImagePullPolicyToJob(job *batchv1.Job) *batchv1.Job {
+func InjectImagePullPolicyForPodSpec(podSpec *corev1.PodSpec) *corev1.PodSpec {
 
-	placeImagePullPolicyToJobs(job)
+	placeImagePullPolicyForPodSpec(podSpec)
 
-	return job
+	return podSpec
 }
 
-func placeImagePullPolicyToPods(pod *corev1.Pod) {
+func placeImagePullPolicy(pod *corev1.Pod) {
 
 	for k, container := range pod.Spec.Containers {
 		container.ImagePullPolicy = corev1.PullIfNotPresent
@@ -28,11 +27,11 @@ func placeImagePullPolicyToPods(pod *corev1.Pod) {
 
 }
 
-func placeImagePullPolicyToJobs(job *batchv1.Job) {
+func placeImagePullPolicyForPodSpec(podSpec *corev1.PodSpec) {
 
-	for k, container := range job.Spec.Template.Spec.Containers {
+	for k, container := range podSpec.Containers {
 		container.ImagePullPolicy = corev1.PullIfNotPresent
-		job.Spec.Template.Spec.Containers[k] = container
+		podSpec.Containers[k] = container
 	}
 
 }
