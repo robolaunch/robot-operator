f8f063b912c05569715f909aab0e28a2f64b6b66
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-10-03 10:42:31 +0300

    refactor(inject): add config injections for volume configurations

--- internal/configure/mount.go
+++ internal/configure/mount.go
@@ -2,9 +2,82 @@ package configure
 
 import (
 	robotv1alpha1 "github.com/robolaunch/robot-operator/pkg/api/roboscale.io/v1alpha1"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
+func (cfg *ContainerConfigInjector) InjectVolumeMountConfiguration(container *corev1.Container, robot robotv1alpha1.Robot, mountPrefix string) *corev1.Container {
+
+	container.VolumeMounts = append(container.VolumeMounts, getVolumeMountsForPersistentDirs(robot, "")...)
+
+	return container
+}
+
+func (cfg *PodConfigInjector) InjectVolumeConfiguration(pod *corev1.Pod, robot robotv1alpha1.Robot) *corev1.Pod {
+
+	pod.Spec.Volumes = append(pod.Spec.Volumes, getVolumesForPersistentDirs(robot)...)
+
+	return pod
+}
+
+func (cfg *JobConfigInjector) InjectVolumeConfiguration(job *batchv1.Job, robot robotv1alpha1.Robot) *batchv1.Job {
+
+	podSpec := job.Spec.Template.Spec
+
+	podSpec.Volumes = append(podSpec.Volumes, getVolumesForPersistentDirs(robot)...)
+
+	job.Spec.Template.Spec = podSpec
+
+	return job
+}
+
+func getVolumesForPersistentDirs(robot robotv1alpha1.Robot) []corev1.Volume {
+
+	volumes := []corev1.Volume{}
+	for _, pDir := range robot.Status.PersistentDirectories {
+		volumes = append(volumes, getVolumeForPersistentDir(pDir))
+	}
+
+	return volumes
+}
+
+func getVolumeMountsForPersistentDirs(robot robotv1alpha1.Robot, mountPrefix string) []corev1.VolumeMount {
+
+	volumeMounts := []corev1.VolumeMount{}
+	for _, pDir := range robot.Status.PersistentDirectories {
+		volumeMounts = append(volumeMounts, getVolumeMountForPersistentDir(mountPrefix, pDir))
+	}
+
+	return volumeMounts
+}
+
+func getVolumeForPersistentDir(pDir robotv1alpha1.PersistentDirectory) corev1.Volume {
+	volume := corev1.Volume{
+		Name: pDir.Status.Reference.Name,
+		VolumeSource: corev1.VolumeSource{
+			PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+				ClaimName: pDir.Status.Reference.Name,
+			},
+		},
+	}
+
+	return volume
+}
+
+func getVolumeMountForPersistentDir(
+	mountPrefix string,
+	pDir robotv1alpha1.PersistentDirectory,
+) corev1.VolumeMount {
+
+	volumeMount := corev1.VolumeMount{
+		Name:      pDir.Status.Reference.Name,
+		MountPath: mountPrefix + pDir.Path,
+	}
+
+	return volumeMount
+}
+
+// *Deprecated*
 func GetVolumeVar(robot *robotv1alpha1.Robot) corev1.Volume {
 
 	volume := corev1.Volume{
@@ -19,6 +92,7 @@ func GetVolumeVar(robot *robotv1alpha1.Robot) corev1.Volume {
 	return volume
 }
 
+// *Deprecated*
 func GetVolumeOpt(robot *robotv1alpha1.Robot) corev1.Volume {
 
 	volume := corev1.Volume{
@@ -33,6 +107,7 @@ func GetVolumeOpt(robot *robotv1alpha1.Robot) corev1.Volume {
 	return volume
 }
 
+// *Deprecated*
 func GetVolumeUsr(robot *robotv1alpha1.Robot) corev1.Volume {
 
 	volume := corev1.Volume{
@@ -47,6 +122,7 @@ func GetVolumeUsr(robot *robotv1alpha1.Robot) corev1.Volume {
 	return volume
 }
 
+// *Deprecated*
 func GetVolumeEtc(robot *robotv1alpha1.Robot) corev1.Volume {
 
 	volume := corev1.Volume{
@@ -115,6 +191,7 @@ func GetVolumeXglCache() corev1.Volume {
 	return volume
 }
 
+// *Deprecated*
 func GetVolumeMount(
 	mountPrefix string,
 	volume corev1.Volume,
