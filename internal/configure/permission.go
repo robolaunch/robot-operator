f78247ffb29cf26116e3c28df761571ac3d35b97
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-10-16 13:05:31 +0300

    refactor(permissions): generate cmd for granting rwx permissions to user

---
+++ internal/configure/permission.go
@@ -0,0 +1,25 @@
+package configure
+
+import (
+	"strings"
+
+	"github.com/robolaunch/robot-operator/internal"
+	robotv1alpha1 "github.com/robolaunch/robot-operator/pkg/api/roboscale.io/v1alpha1"
+)
+
+func GetGrantPermissionCmd(robot robotv1alpha1.Robot) string {
+
+	var permissionCmdBuilder strings.Builder
+	var closerStr string = ";"
+
+	var paths []string
+	if val, ok := robot.Spec.AdditionalConfigs[internal.GRANT_PERMISSION_KEY]; ok && val.ConfigType == robotv1alpha1.AdditionalConfigTypeOperator {
+		paths = strings.Split(val.Value, ":")
+	}
+
+	for _, v := range paths {
+		permissionCmdBuilder.WriteString("sudo setfacl -R -m u:robolaunch:rwx " + v + closerStr + " ")
+	}
+
+	return permissionCmdBuilder.String()
+}
