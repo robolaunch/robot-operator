18221cbd406531b40611a2498748b704cda169b3
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-12-05 16:11:03 +0300

    refactor(timezone): extend timezone config module w/ jobs

--- internal/configure/timezone.go
+++ internal/configure/timezone.go
@@ -2,6 +2,7 @@ package configure
 
 import (
 	"github.com/robolaunch/robot-operator/internal/label"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -10,6 +11,13 @@ func (cfg *PodConfigInjector) InjectTimezone(pod *corev1.Pod, node corev1.Node) 
 	cfg.placeTimezone(pod, node)
 
 	return pod
+}
+
+func (cfg *JobConfigInjector) InjectTimezone(job *batchv1.Job, node corev1.Node) *batchv1.Job {
+
+	cfg.placeTimezone(job, node)
+
+	return job
 }
 
 func (cfg *PodConfigInjector) placeTimezone(pod *corev1.Pod, node corev1.Node) {
@@ -39,5 +47,39 @@ func (cfg *PodConfigInjector) placeTimezone(pod *corev1.Pod, node corev1.Node) {
 		container.VolumeMounts = append(container.VolumeMounts, volumeMount)
 		pod.Spec.Containers[k] = container
 	}
+
+}
+
+func (cfg *JobConfigInjector) placeTimezone(job *batchv1.Job, node corev1.Node) {
+
+	podSpec := job.Spec.Template.Spec
+
+	timezone := label.GetTimezone(&node)
+	if timezone.Continent == "" || timezone.City == "" {
+		return
+	}
+
+	volume := corev1.Volume{
+		Name: "tz-config",
+		VolumeSource: corev1.VolumeSource{
+			HostPath: &corev1.HostPathVolumeSource{
+				Path: "/usr/share/zoneinfo/" + timezone.Continent + "/" + timezone.City,
+			},
+		},
+	}
+
+	podSpec.Volumes = append(podSpec.Volumes, volume)
+
+	volumeMount := corev1.VolumeMount{
+		Name:      "tz-config",
+		MountPath: "/etc/localtime",
+	}
+
+	for k, container := range podSpec.Containers {
+		container.VolumeMounts = append(container.VolumeMounts, volumeMount)
+		podSpec.Containers[k] = container
+	}
+
+	job.Spec.Template.Spec = podSpec
 
 }
