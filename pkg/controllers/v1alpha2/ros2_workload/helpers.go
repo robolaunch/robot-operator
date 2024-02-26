0130dc3ae04cef976b2740721cf71182056f07dc
tunahanertekin <tunahan.ertekin13@gmail.com>
2024-02-26 15:32:41 +0300

    refactor(controller): add template functions

--- pkg/controllers/v1alpha2/ros2_workload/helpers.go
+++ pkg/controllers/v1alpha2/ros2_workload/helpers.go
@@ -1,1 +1,36 @@
 package ros2_workload
+
+import (
+	"context"
+
+	robotv1alpha2 "github.com/robolaunch/robot-operator/pkg/api/roboscale.io/v1alpha2"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/retry"
+)
+
+func (r *ROS2WorkloadReconciler) reconcileGetInstance(ctx context.Context, meta types.NamespacedName) (*robotv1alpha2.ROS2Workload, error) {
+	instance := &robotv1alpha2.ROS2Workload{}
+	err := r.Get(ctx, meta, instance)
+	if err != nil {
+		return &robotv1alpha2.ROS2Workload{}, err
+	}
+
+	return instance, nil
+}
+
+func (r *ROS2WorkloadReconciler) reconcileUpdateInstanceStatus(ctx context.Context, instance *robotv1alpha2.ROS2Workload) error {
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		instanceLV := &robotv1alpha2.ROS2Workload{}
+		err := r.Get(ctx, types.NamespacedName{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		}, instanceLV)
+
+		if err == nil {
+			instance.ResourceVersion = instanceLV.ResourceVersion
+		}
+
+		err1 := r.Status().Update(ctx, instance)
+		return err1
+	})
+}
--- pkg/controllers/v1alpha2/ros2_workload/ros2workload_controller.go
+++ pkg/controllers/v1alpha2/ros2_workload/ros2workload_controller.go
@@ -19,11 +19,13 @@ package ros2_workload
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	robotv1alpha2 "github.com/robolaunch/robot-operator/pkg/api/roboscale.io/v1alpha2"
 )
 
@@ -37,9 +39,61 @@ type ROS2WorkloadReconciler struct {
 //+kubebuilder:rbac:groups=robot.roboscale.io,resources=ros2workloads/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=robot.roboscale.io,resources=ros2workloads/finalizers,verbs=update
 
+var logger logr.Logger
+
 func (r *ROS2WorkloadReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-	return ctrl.Result{}, nil
+	logger = log.FromContext(ctx)
+	logger.Info("Starting reconciler...")
+
+	var result ctrl.Result = ctrl.Result{}
+
+	instance, err := r.reconcileGetInstance(ctx, req.NamespacedName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// ***reconciler logic***
+	// get instance
+	// check node
+	// check persistent directories
+	// check host directories
+	// check status
+	// update status
+	// check resource
+	// update status
+
+	err = r.reconcileCheckStatus(ctx, instance, &result)
+	if err != nil {
+		return result, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileCheckResources(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return result, nil
+}
+
+func (r *ROS2WorkloadReconciler) reconcileCheckStatus(ctx context.Context, instance *robotv1alpha2.ROS2Workload, result *ctrl.Result) error {
+	return nil
+}
+
+func (r *ROS2WorkloadReconciler) reconcileCheckResources(ctx context.Context, instance *robotv1alpha2.ROS2Workload) error {
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
