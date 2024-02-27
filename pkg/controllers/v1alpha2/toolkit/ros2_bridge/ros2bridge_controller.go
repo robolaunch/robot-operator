4e7fcf155509d197c3d66fceb0f90758dbf5492b
tunahanertekin <tunahan.ertekin13@gmail.com>
2024-02-27 11:43:47 +0300

    refactor(controller): implement legacy ros2bridge reconciler logic

--- pkg/controllers/v1alpha2/toolkit/ros2_bridge/ros2bridge_controller.go
+++ pkg/controllers/v1alpha2/toolkit/ros2_bridge/ros2bridge_controller.go
@@ -14,16 +14,21 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package controllers
+package ros2_bridge
 
 import (
 	"context"
 
+	robotErr "github.com/robolaunch/robot-operator/internal/error"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	robotv1alpha2 "github.com/robolaunch/robot-operator/pkg/api/roboscale.io/v1alpha2"
 )
 
@@ -37,14 +42,96 @@ type ROS2BridgeReconciler struct {
 //+kubebuilder:rbac:groups=robot.roboscale.io,resources=ros2bridges/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=robot.roboscale.io,resources=ros2bridges/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=robot.roboscale.io,resources=discoveryservers,verbs=get;list;watch
+
+var logger logr.Logger
+
 func (r *ROS2BridgeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-	return ctrl.Result{}, nil
+	logger = log.FromContext(ctx)
+
+	var result ctrl.Result = ctrl.Result{}
+
+	instance, err := r.reconcileGetInstance(ctx, req.NamespacedName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileCheckStatus(ctx, instance, &result)
+	if err != nil {
+		return result, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileCheckResources(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.reconcileUpdateInstanceStatus(ctx, instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	return result, nil
+}
+
+func (r *ROS2BridgeReconciler) reconcileCheckStatus(ctx context.Context, instance *robotv1alpha2.ROS2Bridge, result *ctrl.Result) error {
+
+	err := r.reconcileHandleService(ctx, instance)
+	if err != nil {
+		return robotErr.CheckCreatingOrWaitingError(result, err)
+	}
+
+	err = r.reconcileHandlePod(ctx, instance)
+	if err != nil {
+		return robotErr.CheckCreatingOrWaitingError(result, err)
+	}
+
+	err = r.reconcileHandleIngress(ctx, instance)
+	if err != nil {
+		return robotErr.CheckCreatingOrWaitingError(result, err)
+	}
+
+	instance.Status.Phase = robotv1alpha2.ROS2BridgePhaseReady
+
+	return nil
+}
+
+func (r *ROS2BridgeReconciler) reconcileCheckResources(ctx context.Context, instance *robotv1alpha2.ROS2Bridge) error {
+
+	err := r.reconcileCheckService(ctx, instance)
+	if err != nil {
+		return err
+	}
+
+	err = r.reconcileCheckPod(ctx, instance)
+	if err != nil {
+		return err
+	}
+
+	err = r.reconcileCheckIngress(ctx, instance)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ROS2BridgeReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&robotv1alpha2.ROS2Bridge{}).
+		Owns(&corev1.Pod{}).
+		Owns(&corev1.Service{}).
+		Owns(&networkingv1.Ingress{}).
 		Complete(r)
 }
