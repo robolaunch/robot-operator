66b53326225da1e3c45f2913c7347a7b7761408d
tunahanertekin <tunahan.ertekin13@gmail.com>
2024-03-04 11:33:23 +0300

    refactor(volume): convert volume config to pod config injection

--- internal/configure/v1alpha2/volume.go
+++ internal/configure/v1alpha2/volume.go
@@ -2,17 +2,16 @@ package configure
 
 import (
 	robotv1alpha2 "github.com/robolaunch/robot-operator/pkg/api/roboscale.io/v1alpha2"
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
-func (cfg *StatefulSetConfigInjector) InjectVolumeConfiguration(statefulSet *appsv1.StatefulSet, ros2Workload robotv1alpha2.ROS2Workload) {
-	cfg.injectOwnedPVCVolumes(statefulSet, ros2Workload)
+func (cfg *PodConfigInjector) InjectVolumeConfiguration(pod *corev1.Pod, ros2Workload robotv1alpha2.ROS2Workload) {
+	cfg.injectOwnedPVCVolumes(pod, ros2Workload)
 }
 
-func (cfg *StatefulSetConfigInjector) injectOwnedPVCVolumes(statefulSet *appsv1.StatefulSet, ros2Workload robotv1alpha2.ROS2Workload) {
+func (cfg *PodConfigInjector) injectOwnedPVCVolumes(pod *corev1.Pod, ros2Workload robotv1alpha2.ROS2Workload) {
 	for _, pvcStatus := range ros2Workload.Status.PVCStatuses {
-		statefulSet.Spec.Template.Spec.Volumes = append(statefulSet.Spec.Template.Spec.Volumes, corev1.Volume{
+		pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
 			Name: pvcStatus.Resource.Reference.Name,
 			VolumeSource: corev1.VolumeSource{
 				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
